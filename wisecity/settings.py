59d9eeb3a22e79a89e8ac42b5d9309acbd624dde
Marat2010 <marat.s.73@mail.ru>
2021-01-30 00:04:53 +0300

    static

--- wisecity/settings.py
+++ wisecity/settings.py
@@ -138,7 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    ('', os.path.join(BASE_DIR, 'pulsar/static')),
-]
+# STATICFILES_DIRS = [
+#     ('', os.path.join(BASE_DIR, 'pulsar/static')),
+# ]
 
