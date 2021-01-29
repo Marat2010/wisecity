b3b66442153902dd5f82858af376d13999bc2240
Marat2010 <marat.s.73@mail.ru>
2021-01-29 21:35:13 +0300

    added index

--- pulsar/views.py
+++ pulsar/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'pulsar/index.html')
+    # return render(request, 'HTML/index.html')
+
+
+# ----------------------
+# def index(request):
+#     return HttpResponse("<h1>Wise City Pulsar </h1>")
+
+
