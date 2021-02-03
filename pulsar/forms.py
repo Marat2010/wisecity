a07217a23ef8c2eb2538e54f586c4d751e5ebf9b
Marat2010 <marat.s.73@mail.ru>
2021-02-03 04:36:59 +0300

    Added a form on the site https://wisecity.herokuapp.com/

--- pulsar/forms.py
+++ pulsar/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class PulsarForm(forms.Form):
     request_bytes = forms.CharField(label='Запрос (request_bytes)', max_length=300)
-    response_bytes = forms.CharField(label='Ответ (response_bytes)', max_length=300)
+    response_bytes = forms.CharField(label='Ответ (response_bytes)', max_length=500)
