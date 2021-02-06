e374c159a1bb46c20807531fb1b69f27964cc0de
Marat2010 <marat.s.73@mail.ru>
2021-02-06 14:08:13 +0300

    replacement with struct

--- pulsar/task.py
+++ pulsar/task.py
@@ -24,7 +24,8 @@
 '''
 
 import json
-
+import struct
+import math
 
 request_bytes = bytearray([0x02, 0x27, 0x21, 0x35, 0x01, 0x0e, 0xfc, 0x3f, 0x08, 0x00, 0xba, 0x1c, 0x49, 0x73])
 response_bytes = bytearray([
@@ -97,19 +98,25 @@ def parsing_request_mask(mask: bytearray) -> tuple:
     mask_bin = [(bin(i)[2:]).rjust(8, '0') for i in reversed(mask)]
     mask_bin = ''.join(mask_bin)  # => '00000000000010000011111111111100'
     ch_list = []  # список каналов, будем добавлять номера в них
-    [ch_list.append(_ + 1) for _, bit_val in enumerate(mask_bin[::-1]) if bit_val == '1']
+    [ch_list.append(i + 1) for i, bit_val in enumerate(mask_bin[::-1]) if bit_val == '1']
     # ch_list => [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 20]
     return tuple(ch_list)
 
 
-def data_split(data_bytes: bytearray) -> tuple:
-    """ Разбиение данных "DATA" для каждого параметра (по 4 байта для канала) """
+def data_split_parsing(data_bytes: bytearray) -> tuple:
+    """ Разбиение данных "DATA" для каждого показания счетчика (по 4 байта для канала)
+     и преобразование в  в человеко-читаемый вид """
     values = []
-
     number_of_value = int(len(data_bytes)/4)  # Кол-во параметров(каналов) значений (одно значение = 4 байта)
     for i in range(1, number_of_value+1, 1):  # Берем сразу по 4 байта [0:4], [4:8] [8:12] ...
-        values.append(tuple(data_bytes[i*4-4:i*4]))  # => [(149, 19, 97, 66), ... ]
-
+        if i != number_of_value:
+            val = struct.unpack('<f', data_bytes[i * 4 - 4:i * 4])[0]  # Преобразование в число с плав.запятой.
+            val = round(val, 3) if not math.isnan(val) else 0.0  # если значение "Nan"
+        else:  # для "normalWorkTime" => выдает "11628", в задании д.б. = "0.0"
+            val = struct.unpack('I', data_bytes[i * 4 - 4:i * 4])[0]  # Преобраз-ие "для normalWorkTime" (=11628).
+            # Для преобраз-ие "normalWorkTime" = 0.0, раскомментировать строку ниже
+            # val = round(struct.unpack('<f', data_bytes[i * 4 - 4:i * 4])[0], 3)
+        values.append(val)
     return tuple(values)
 
 
@@ -123,32 +130,6 @@ def check_package(package: dict) -> bool:
     return True
 
 
-def get_readable_data(channel_data: tuple) -> float:
-    """ Получает кортеж их 4-х байт, возвращает значение показания счетчика в человеко-читаемом виде.
-    Формула вычисления взята отсюда: https://www.softelectro.ru/ieee754.html .
-        Чтобы записать число в стандарте IEEE 754 или восстановить его, необходимо знать три параметра:
-            S- бит знака (31-й бит)
-            E- смещенная экспонента (30-23 биты)
-            M - остаток от мантиссы (22-0 биты) """
-
-    list_bytes = [(bin(i)[2:]).rjust(8, '0') for i in reversed(channel_data)]
-    # переводим в двоичный формат младшим байтом вперёд
-    # (149, 19, 97, 66) => list_bytes = ['01000010', '01100001', '00010011', '10010101']
-
-    bits = ''.join(list_bytes)  # => 01000010011000010001001110010101
-
-    sign_bit_S = int(bits[0])  # бит знака (31-й бит => 0 бит). => "=0"
-    exp_E = int(bits[1:9], 2)  # Смещенная экспонента E (30-23 биты => 1-8 биты). => "=132"
-    mantissa_M = int(bits[9:], 2)  # Остаток от мантиссы M (22-0 биты => 9-30 биты). => "=6362005"
-
-    value = ((-1) ** sign_bit_S) * 2**(exp_E - 127) * (1 + mantissa_M / 2**23)  # формула расчета
-
-    if value < 1e-38 or value > 1e+38:  # Отсечь -6.805646...e+38 => -∞ (0xff, ...) и 7.1333939...e-39 => 0 (0x00,  ...)
-        value = 0.0
-    value = round(value, 3)
-    return value
-
-
 def parse_response(response: bytearray) -> dict:
     """Ваша реализация здесь"""
     request_package = package_split(request_bytes)  # Распаковка пакета ЗАПРОСА (request) для получение маски
@@ -160,14 +141,11 @@ def parse_response(response: bytearray) -> dict:
     # функцию "check_package" необходимо расширить для проверки адресов "ADDR", кода функции "F" в двух
     # пакетах (request, response), вычисление CRC16.
 
-    channels_data = data_split(package['DATA'])  # Разбиение данных "DATA" для каждого параметра (4 байта на канал)
-
-    channels_values = []  # список показаний счетчика в человеко-читаемом виде
-    [channels_values.append(get_readable_data(_)) for _ in channels_data]  # => [56.269, 45.59, 10.68, ..., 0.0]
+    channels_values = data_split_parsing(package['DATA'])  # Разбиение данных "DATA" (4 байта на канал), и парсинг
 
     number_value = tuple(zip(channels, channels_values))  # кортеж (номер канала-значение) => ((3, 56.269)... (20, 0.0))
 
-    sensorId = ''.join([hex(_)[2:] for _ in package['ADDR']])  # адрес устройства (4байта) в формате BCD
+    sensorId = ''.join([hex(byte)[2:] for byte in package['ADDR']])  # адрес устройства (4байта) в формате BCD
     result = {'sensorId': sensorId}  # Результируюший словарь
 
     for i, ch in enumerate(number_value):  # Добавление в словарь значений каналов
@@ -182,3 +160,29 @@ if __name__ == '__main__':
     print(f'***** РЕЗУЛЬТАТ **** :')
     print(json.dumps(request, indent=4, sort_keys=True))
 
+
+# ------------ Переделан с использованием Struct (Было по формуле) -------------------------------------------
+# def get_readable_data(channel_data: tuple) -> float:
+#     """ Получает кортеж их 4-х байт, возвращает значение показания счетчика в человеко-читаемом виде.
+#     Формула вычисления взята отсюда: https://www.softelectro.ru/ieee754.html .
+#         Чтобы записать число в стандарте IEEE 754 или восстановить его, необходимо знать три параметра:
+#             S- бит знака (31-й бит)
+#             E- смещенная экспонента (30-23 биты)
+#             M - остаток от мантиссы (22-0 биты) """
+#
+#     list_bytes = [(bin(i)[2:]).rjust(8, '0') for i in reversed(channel_data)]
+#     # переводим в двоичный формат младшим байтом вперёд
+#     # (149, 19, 97, 66) => list_bytes = ['01000010', '01100001', '00010011', '10010101']
+#
+#     bits = ''.join(list_bytes)  # => 01000010011000010001001110010101
+#
+#     sign_bit_S = int(bits[0])  # бит знака (31-й бит => 0 бит). => "=0"
+#     exp_E = int(bits[1:9], 2)  # Смещенная экспонента E (30-23 биты => 1-8 биты). => "=132"
+#     mantissa_M = int(bits[9:], 2)  # Остаток от мантиссы M (22-0 биты => 9-30 биты). => "=6362005"
+#
+#     value = ((-1) ** sign_bit_S) * 2**(exp_E - 127) * (1 + mantissa_M / 2**23)  # формула расчета
+#
+#     if value < 1e-38 or value > 1e+38:  # Отсечь -6.805646...e+38 => -∞ (0xff, ...) и 7.1333939...e-39 => 0 (0x00,  ...)
+#         value = 0.0
+#     value = round(value, 3)
+#     return value
