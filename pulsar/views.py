b433affe214d9fcc37aa1357afeb5ee879355fe6
Marat2010 <marat.s.73@mail.ru>
2021-03-23 02:23:36 +0300

    go to dev

--- pulsar/views.py
+++ pulsar/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
-from .forms import PulsarForm
+from .forms import PulsarForm, PulsarForm2
 from .task import parse_response
 import json
 
 
 def pulsar(request):
+    # generated = {'initial': 'ff ff ff'}
+    generated = 'ff ff ff'
     if request.method == 'POST':
-        form = PulsarForm(request.POST)
+
+        # form = PulsarForm(request.POST, {'initial': generated})
+        generated = 'aa aa 00 000 000'
+        # form = PulsarForm2(request.POST, initial={'initial': generated})
+        form = PulsarForm2(request.POST, initial={'initial': generated})
         if form.is_valid():
             request_bytes = form.data['request_bytes']
+            # request_bytes = '11 111 11 11 aa aa 00 000 000'
             response_bytes = form.data['response_bytes']
 
             print(f"--- Запрос request_bytes --- {request_bytes}")
@@ -25,12 +32,32 @@ def pulsar(request):
                 data_error = True
             print(f"---result----: {json.dumps(result, indent=4, sort_keys=True)}")
 
-            context = {'form': form, 'result': result, 'data_error': data_error}
+            # form['request_bytes'] .initial = {'initial': 212212}
+            # form.data.initial = {'initial': 21221211111111111111111}
+            print(form.data)
+            # print(form.data['request_bytes'][0]['initial'])
+            # form.data['request_bytes'] = ["{'initial': 'aa aa 00 000 000'}"]
+            # print(form.request_bytes.initial)
+            # for i, fm in enumerate(form):
+            #     fm = 'ewewewewee'
+            #     print(fm)
+            # form.data['request_bytes'].initial = {'innitial': '0x02, 0x27, 0x21, 0x35'}
+
+            # form['request_bytes'].initial = 'aa aa 11 000 000'
+            print(f'======= {form.initial}')
+            print(f'======= {form.data["request_bytes"]}')
+            # form.initial ['initial'] = "{'initial': 'aa aa 0000000 ff'}"
+            context = {'form': form, 'result': result, 'data_error': data_error, 'generated': generated}
             return render(request, 'pulsar/index.html', context=context)
     else:
-        form = PulsarForm()
+        # form = PulsarForm(request.GET, initial=None)
+        # print(form.request_bytes.generated)
+
+        # form = PulsarForm()
+        form = PulsarForm(initial={'initial': generated})
 
     return render(request, 'pulsar/index.html', {'form': form})
+    # return render(request, 'pulsar/index.html', {'form': form, 'generated': generated})
 
 
 # -------------------------------------------------
